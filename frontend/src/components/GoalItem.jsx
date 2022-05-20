7d4157c591011e3e1935986aa7e69b6fd9ced055
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-20 16:18:16 -0400

    goal styling

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -3,6 +3,19 @@ import { useDispatch } from 'react-redux'
 import { deleteGoal, updateGoal } from '../features/goals/goalSlice'
 import { FaRegEdit, FaTimesCircle, FaSave, FaRegWindowClose } from 'react-icons/fa'
 
+const updateButtonStyle = {
+    border:'none',
+    color:'cornflowerblue',
+    backgroundColor:'transparent'
+}
+
+const saveButtonStyle = {
+    border:'none',
+    color:'cornflowerblue',
+    backgroundColor:'transparent',
+    marginInline:'1rem'
+}
+
 function GoalItem({ goal }) {
     const [text, setText] = useState(goal.text)
     const [showEditForm, setShowEditForm] = useState(false)
@@ -24,7 +37,10 @@ function GoalItem({ goal }) {
             <div style={{color:'gray'}}>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
 
             { showEditForm ? (
-                <form className='goal-edit-form' onSubmit={e => e.preventDefault()}>
+                <form 
+                    className='goal-edit-form' 
+                    onSubmit={e => e.preventDefault()}
+                >
                     <div className='form-group'>
                         <input 
                             type='text'
@@ -33,31 +49,44 @@ function GoalItem({ goal }) {
                             placeholder='Enter goal text'
                             value={text}
                             onChange={e => setText(e.target.value)}
-                            style={{fontSize:'1rem'}}
+                            style={{fontSize:'0.75rem',width:'90%'}}
                         />
                     </div>
                     <p style={{color:'cornflowerblue'}}>Save?</p>
-                    <button type='button' className='goal-save-icon' onClick={onUpdateGoal} style={{border:'none',color:'cornflowerblue',backgroundColor:'transparent',marginInline:'1rem'}}>
+                    <button 
+                        className='goal-save-icon' 
+                        type='button' 
+                        onClick={onUpdateGoal} 
+                        style={saveButtonStyle}
+                    >
                         <FaSave size={25} />
                     </button>
-                    <button type='button' className='goal-skip-save-icon' onClick={() => setShowEditForm(prevState => !prevState)} style={{border:'none',color:'cornflowerblue',backgroundColor:'transparent',marginInline:'1rem'}}>
+                    <button 
+                        className='goal-skip-save-icon' 
+                        type='button' 
+                        onClick={() => setShowEditForm(prevState => !prevState)} style={saveButtonStyle}
+                    >
                         <FaTimesCircle size={25} />
                     </button>
                 </form>
             ) : (
                 <div className="goal-text-wrapper">
                     <hr style={{marginInline:'1rem'}} />
-                    <h2>{goal.text}</h2>
-                    <button className='goal-edit icon' 
-                        style={{border:'none',color:'cornflowerblue',backgroundColor:'transparent'}}
-                        onClick={() => setShowEditForm(prevState => !prevState)}>
+                    <h3>{goal.text}</h3>
+                    <button 
+                        className='goal-edit icon' 
+                        style={updateButtonStyle}
+                        onClick={() => setShowEditForm(prevState => !prevState)}
+                    >
                         <FaRegEdit size={20}/> Update
                     </button>
                 </div>
             )}
-            <button className='close' 
+            <button 
+                className='close' 
                 style={{margin:'-0.5rem',color:'cornflowerblue',}}  
-                onClick={onDeleteGoal}>
+                onClick={onDeleteGoal}
+            >
                 <span>Delete </span><FaRegWindowClose size={20}/>
             </button>
         </div>
